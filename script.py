dd3aa7e183e83eb35c45d5230f26a6993cd8638d
Derek Fan <dfan@it-usa-l01350.attlocal.net>
2020-10-16 14:31:00 -0500

    Initial Commit

---
+++ script.py
@@ -0,0 +1,20 @@
+import math
+from os import name
+import sys
+
+import requests
+#print (sys.version)
+print (sys.executable)
+
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
+
+
+name = input("Your Name?")
+print("Hello,", name)
